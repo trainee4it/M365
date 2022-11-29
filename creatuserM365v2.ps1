6e3eb0dbed4b06539420d45bb1512b11e08d9f79
trainee4it <69849181+trainee4it@users.noreply.github.com>
2022-11-29 14:52:45 +0100

    Add files via upload

---
+++ creatuserM365v2.ps1
@@ -0,0 +1,28 @@
+﻿if(!(Get-MsolAccountSku))
+{
+
+
+    Connect-MsolService
+
+}
+
+
+$OLdUpnSuffix = '@contoso.com'
+$NewUPnSuffix = '@M365x84644147.onmicrosoft.com'
+
+
+
+$AllUsers = Import-Csv -Path C:\test\Import.csv
+
+foreach($user in $AllUsers)
+{
+    
+    if($user.Name -ne 'cloud11' -and $user.Name -ne 'cloud12')
+    {
+
+        New-MsolUser -UserPrincipalName ($user.Name + $NewUPnSuffix) -DisplayName $user.Name  -usageLocation 'nl' -Password $user.password -LicenseAssignment (Get-MsolAccountSku).accountskuid[2]
+
+    }
+
+}
+
