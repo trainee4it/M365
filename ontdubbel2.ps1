819e93d652bb623a3842cef8ffad1d7a9e9e75be
trainee4it <69849181+trainee4it@users.noreply.github.com>
2022-11-29 11:18:50 +0100

    Add files via upload

---
+++ ontdubbel2.ps1
@@ -0,0 +1,53 @@
+﻿$Original = Import-Csv -Path C:\right\fault.csv
+
+$Reference =  Import-Csv -Path C:\right\fault.csv
+
+$TargetArray = @()
+
+if(Test-Path -Path C:\right\correct.csv)
+{
+
+    Remove-Item -Path C:\right\correct.csv
+
+}
+foreach($item in $Original)
+{
+      $Done = $false
+    
+       
+      foreach($line in $TargetArray)
+      {
+        
+            
+            if($item.FIRSTNAME -eq $line.Firstname -and $item.LASTNAME -eq $line.LASTNAME)
+            {
+
+                
+                $Done = $true
+               
+            }
+
+
+      }
+
+      if(!($Done))
+      {
+
+        $hash = @{ "FIRSTNAME" = $item.FIRSTNAME; "LASTNAME"= $item.LASTNAME; "DESCRIPTION" = $item.DESCRIPTION}
+
+        $Object = New-Object -TypeName psobject -Property $hash
+
+        $TargetArray += $Object
+
+        $Object | Export-Csv -Path 'c:\right\correct.csv' -Append -NoTypeInformation -Delimiter ','
+
+        Write-Verbose -Message("User " +$item.firstname + " exists 1 times") -Verbose 
+
+
+       }
+        
+
+        
+
+    
+}
