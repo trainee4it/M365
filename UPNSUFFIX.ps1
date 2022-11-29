51fc5a14e2680e212daa0f711347086735927661
trainee4it <69849181+trainee4it@users.noreply.github.com>
2022-11-29 00:26:51 -0800

    Add files via upload

---
+++ UPNSUFFIX.ps1
@@ -0,0 +1,16 @@
+﻿$OLdUpnSuffix = '@contoso.com'
+$NewUPnSuffix = '@sendmegb.tk'
+
+
+
+$AllUsers = Get-ADUser -Filter *
+
+foreach($user in $AllUsers)
+{
+
+    $user| Set-ADUser -UserPrincipalName ($user.Samaccountname + $NewUPnSuffix)
+
+
+
+}
+
