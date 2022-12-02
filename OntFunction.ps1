433cbf82d6647b23ad3d61c6b3b2c349fe750e22
Trainee4it <Trainee4it@outlook.com>
2022-12-02 15:02:17 +0100

    Added File

---
+++ OntFunction.ps1
@@ -0,0 +1,87 @@
+
+if(!(Test-Path -Path 'C:\right'))
+{
+
+    New-Item -Path 'C:\right' -ItemType Directory
+
+}
+
+
+Write-Host "Did you place a file in the c:\right folder named fault.csv  (j/n) "
+$Answer = Read-Host
+while($Answer -eq 'n')
+{
+
+    Write-Host "Please place a file to be deduplicated in the C:\right folder named fault.csv and press 'y'"
+    $Answer = Read-Host
+
+}
+
+
+
+
+if(Test-Path -Path C:\right\correct.csv)
+{
+
+    Remove-Item -Path C:\right\correct.csv
+
+}
+
+$Original = Import-Csv -Path C:\right\fault.csv
+
+$Reference =  Import-Csv -Path C:\right\fault.csv
+
+$TargetArray = New-Object System.Collections.ArrayList
+
+foreach($item in $Original)
+{
+      $Done = $false
+    
+       
+      foreach($line in $TargetArray)
+      {
+        
+            
+            if($item.FIRSTNAME -eq $line.Firstname -and $item.LASTNAME -eq $line.LASTNAME)
+            {
+
+                
+                $Done = $true
+               
+            }
+
+
+
+      }
+
+      if($Done)
+      {
+
+
+        Write-Verbose -Message("User " +$item.firstname + " exists more times") -Verbose 
+
+
+      }
+
+      if(!($Done))
+      {
+        $FirstName = $item.firstname
+        $LastName = $item.lastname
+        $hash = [ordered]@{"FIRSTNAME" = $FirstName; "LASTNAME"= $LastName} 
+
+        $Object = New-Object -TypeName psobject -Property $hash 
+
+        $TargetArray.Add($Object) | Out-Null
+
+        $Object | Export-Csv -Path 'c:\right\correct.csv' -Append -NoTypeInformation -Delimiter ',' 
+
+       
+
+
+       }
+        
+
+        
+
+    
+}
